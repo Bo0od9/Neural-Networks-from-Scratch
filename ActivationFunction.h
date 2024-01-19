26f891f641c27b0380138e12cd12dfa0412b150d
Ivan Pleshkov <el3bry@gmail.com>
2024-01-19 15:01:18 +0300

    Fix SigmoidActivation.h ActivationFunction.h

--- ActivationFunction.h
+++ ActivationFunction.h
@@ -1,16 +1,14 @@
 #ifndef ACTIVATIONFUNCTION_H_
 #define ACTIVATIONFUNCTION_H_
-#include "Eigen/Dense"
+#include <cmath>
+
 namespace Network {
 class ActivationFunction {
 
  public:
-  using Matrix = Eigen::MatrixXd;
-  using Vector = Eigen::VectorXd;
-
   virtual ~ActivationFunction() = default;
-  [[nodiscard]] virtual double compute(double x) const = 0;
-  [[nodiscard]] virtual double computeDerivative(double x) const = 0;
+  virtual double compute(double x) const = 0;
+  virtual double computeDerivative(double x) const = 0;
 };
 }// namespace Network
 #endif//ACTIVATIONFUNCTION_H_
--- SigmoidActivation.h
+++ SigmoidActivation.h
@@ -1,16 +1,13 @@
 #ifndef SIGMOIDACTIVATION_H_
 #define SIGMOIDACTIVATION_H_
-
 #include "ActivationFunction.h"
-#include <cmath>
 
-//This will be reworked later using erasure types
 namespace Network {
 class SigmoidActivation : public ActivationFunction {
-  [[nodiscard]] double compute(double x) const override {
+  double compute(double x) const override {
     return 1.0 / (1.0 + std::exp(-x));
   }
-  [[nodiscard]] double computeDerivative(double x) const override {
+  double computeDerivative(double x) const override {
     double sigmoid = compute(x);
     return sigmoid * (1 - sigmoid);
   }
