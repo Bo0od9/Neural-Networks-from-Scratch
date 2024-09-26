45af83a62fb442820fa3f384a3bc2e42bad33fac
Ivan Pleshkov <el3bry@gmail.com>
2024-09-26 20:54:58 +0300

    Add declarations.h

---
+++ declarations.h
@@ -0,0 +1,10 @@
+#ifndef DECLARATIONS_H_
+#define DECLARATIONS_H_
+
+#include "Eigen/Dense"
+
+using Index = Eigen::Index;
+using Matrix = Eigen::MatrixXd;
+using Vector = Eigen::VectorXd;
+
+#endif // DECLARATIONS_H_
